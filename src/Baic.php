90277873f2e159e38418ad28ef3c8e3eef72755a
GanGanGan <520125964@qq.com>
2018-08-26 00:00:35 +0800

    add new api

--- src/Baic.php
+++ src/Baic.php
@@ -20,8 +20,12 @@ use Gangangan\Baic\Exceptions\HttpException;
 class Baic
 {
     protected $apiPrefix = 'http://140.143.225.189/paygateway';
-    protected $apiGetToken = '/sdk/getToken';
-    protected $apiGetPorder = '/sdk/getPorder';
+    protected $apiGetToken = '/sdk/getToken';                                           //获取Token
+    protected $apiGetPorder = '/sdk/getPorder';                                         //获取Porder
+    protected $apiSelectOrderByTime = '/sdk/selectOrderByTime';                         //时间区间订单查询
+    protected $apiSelectOrderByPage = '/sdk/selectOrderByPage';                         //订单分页查询
+    protected $apiSelectRefundByTransactionNo = '/sdk/selectRefundByTransactionNo';     //根据交易号退款查询
+    protected $apiSelectByRefundRecordByPage = '/sdk/selectByRefundRecordByPage';       //退款订单分页查询
 
     /**
      * @var string
@@ -39,7 +43,7 @@ class Baic
     protected $guzzleOptions = [];
 
     /**
-     * Weather constructor.
+     * Baic constructor.
      *
      * @param string $sdkId
      * @param string $appKey
@@ -67,8 +71,8 @@ class Baic
     }
 
     /**
+     * 获取Token
      * @return \Psr\Http\Message\ResponseInterface
-     *
      * @throws \Gangangan\Baic\Exceptions\HttpException
      */
     public function getToken()
@@ -92,20 +96,80 @@ class Baic
     }
 
     /**
+     * 获取Porder
      * @param array $data
      * @return \Psr\Http\Message\ResponseInterface
-     *
      * @throws \Gangangan\Baic\Exceptions\HttpException
      */
     public function getPorder($data)
     {
         $url = $this->apiPrefix . $this->apiGetPorder;
+        return $this->doPostFormUrlEncoded($url, $data);
+    }
+
+    /**
+     * 时间区间订单查询
+     * @param array $data
+     * @return \Psr\Http\Message\ResponseInterface
+     * @throws \Gangangan\Baic\Exceptions\HttpException
+     */
+    public function selectOrderByTime($data)
+    {
+        $url = $this->apiPrefix . $this->apiSelectOrderByTime;
+        return $this->doPostFormUrlEncoded($url, $data);
+    }
+
+    /**
+     * 订单分页查询
+     * @param array $data
+     * @return \Psr\Http\Message\ResponseInterface
+     * @throws \Gangangan\Baic\Exceptions\HttpException
+     */
+    public function selectOrderByPage($data)
+    {
+        $url = $this->apiPrefix . $this->apiSelectOrderByPage;
+        return $this->doPostFormUrlEncoded($url, $data);
+    }
+
+    /**
+     * 根据交易号退款查询
+     * @param array $data
+     * @return \Psr\Http\Message\ResponseInterface
+     * @throws \Gangangan\Baic\Exceptions\HttpException
+     */
+    public function selectRefundByTransactionNo($data)
+    {
+        $url = $this->apiPrefix . $this->apiSelectRefundByTransactionNo;
+        return $this->doPostFormUrlEncoded($url, $data);
+    }
+
+    /**
+     * 退款订单分页查询
+     * @param array $data
+     * @return \Psr\Http\Message\ResponseInterface
+     * @throws \Gangangan\Baic\Exceptions\HttpException
+     */
+    public function selectByRefundRecordByPage($data)
+    {
+        $url = $this->apiPrefix . $this->apiSelectByRefundRecordByPage;
+        return $this->doPostFormUrlEncoded($url, $data);
+    }
+
+    /**
+     * application/x-www-form-urlencoded POST请求
+     * @param string $url
+     * @param array $data
+     * @return \Psr\Http\Message\ResponseInterface
+     * @throws \Gangangan\Baic\Exceptions\HttpException
+     */
+    public function doPostFormUrlEncoded($url, $data)
+    {
         $options = [
             'connect_timeout' => 3,
             'form_params' => $data
         ];
-
         $this->setGuzzleOptions($options);
+
         try {
             $response = $this->getHttpClient()->post($url)->getBody()->getContents();
             return \json_decode($response, true);
